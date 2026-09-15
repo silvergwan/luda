6754a4271d378361d21946762baec3de406c296a
silvergwan <silvergwan@gmail.com>
2026-09-15 17:57:19 +0900

    update: 조건 주어서 확인 복습

--- 02_pandas/dataFrame.py
+++ 02_pandas/dataFrame.py
@@ -8,6 +8,8 @@ data = {
 
 df = pd.DataFrame(data)
 
-is_over = df["안전성"] >= 0.9
+is_over_safe = df["안전성"] >= 0.9
+is_over_naturalness = df["자연스러움"] >= 0.8
 
-print(is_over)
+print(is_over_safe)
+print(is_over_naturalness)
